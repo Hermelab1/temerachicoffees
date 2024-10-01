b8738792a2dcf951599540dbdf69d4b3b6305d9c
HermelaB1 <hermelabirhanu06@gmail.com>
2024-10-01 22:23:05 +0300

    update on resposivenes

--- src/components/data/testimonial.js
+++ src/components/data/testimonial.js
@@ -1,6 +1,6 @@
-const img6 = require('../../asset/img/Coworkers/ehab.jpg');
-const img7 = require('../../asset/img/Coworkers/number9.jpg');
-const img8 = require('../../asset/img/Coworkers/ch.jpg');
+const img6 = require('../../asset/img/Coworkers/ehab.webp');
+const img7 = require('../../asset/img/Coworkers/number9.webp');
+const img8 = require('../../asset/img/Coworkers/ch.webp');
 
 
 export const testimonial = [
@@ -9,20 +9,20 @@ export const testimonial = [
         name: "Ehab Alkaba",
         companyname: "Kanso Coffee GmbH",
         paragraph: 'This coffee is simply outstanding for its flavor and special aroma. Every batch we got was homogeneous, exceptionally fresh and very well processed to bring out the origins real character.',
-        flag: require('../../asset/img/Flag/germany.png'), // Ensure path matches the actual location
+        flag: require('../../asset/img/Flag/germany.webp'), // Ensure path matches the actual location
     },
     {
         photo: img7,
         name: "Thomas",
         companyname: "Number Nine Coffee",
         paragraph: "Temerachi Coffee offers a wide range of high quality Ethiopian coffees. The acidity is balanced with the sweetness of the coffee. The efficiency from the team makes sourcing much easier without traveling there.",
-        flag: require('../../asset/img/Flag/Hongkong.png'), // Adjust the path as needed
+        flag: require('../../asset/img/Flag/Hongkong.webp'), // Adjust the path as needed
     },
     {
         photo: img8,
         name: "Takasaki",
         companyname: "Nisoku HoKou Coffee Roasters",
         paragraph: "For years, I’ve been purchasing coffee from this brand without fail to impress. Their commitment to quality and sustainability shines through in every bag.  Consistency, freshness in each batch is truly incredible.",
-        flag: require('../../asset/img/Flag/Japan.png'), // Make sure this path is correct
+        flag: require('../../asset/img/Flag/Japan.webp'), // Make sure this path is correct
     },
 ];
