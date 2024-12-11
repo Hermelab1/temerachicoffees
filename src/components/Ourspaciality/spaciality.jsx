392c9112a44ae8a58e6d97a4dce5c14dc0bf53ec
HermelaB1 <hermelabirhanu06@gmail.com>
2024-12-11 22:25:24 +0300

    Dec 11 2024 updated

--- src/components/Ourspaciality/spaciality.jsx
+++ src/components/Ourspaciality/spaciality.jsx
@@ -23,7 +23,7 @@ const Speciality = () => {
           const iconClass = `fa-solid ${icons[index % icons.length]} mx-[-70px] md:mx-[-20px] border-[3px] border-[#176756] bg-white text-[#176756] text-[1.7rem] item-center text-center py-3 absolute w-[60px] h-[60px] rounded-full  top-8 ${isEvenIndex ? 'md:right-[-60px] ' : 'md:left-[-60px]'}`;
 
           // Use different margin/padding for smaller screens and add some space below the icon
-          const itemClass = `relative 2xl:w-[39%] xl:w-[45%] lg:w-[43%] md:w-[43%] w-[80%] opacity-0 flex flex-col items-start mb-2 ${isEvenIndex ? 'animate-move-left ml-[4.5rem] 2xl:ml-[7%] xl:ml-[2%] lg:ml-[3%] md:ml-[10%]' : 'animate-move-right ml-[4.5rem] 2xl:ml-[53%] xl:ml-[52%] lg:ml-[53%] md:ml-[52%]'}`;
+          const itemClass = `relative 2xl:w-[39%] xl:w-[45%] lg:w-[43%] md:w-[43%] w-[70%] opacity-0 flex flex-col items-start mb-2 ${isEvenIndex ? 'animate-move-left ml-[4.5rem] 2xl:ml-[7%] xl:ml-[2%] lg:ml-[3%] md:ml-[10%]' : 'animate-move-right ml-[4.5rem] 2xl:ml-[53%] xl:ml-[52%] lg:ml-[53%] md:ml-[52%]'}`;
           
           // Hide the border on smaller devices and only show it on medium and larger devices
           const borderClass = `absolute top-[60px] transform -translate-y-1/2 ${isEvenIndex ? 'right-[-14px] border-l-[15px] border-l-[#176756] border-t-[15px] border-b-[15px] border-transparent' : 'left-[-13px] border-r-[15px] border-r-[#176756] border-t-[15px] border-b-[15px] border-transparent'} md:block hidden`;
