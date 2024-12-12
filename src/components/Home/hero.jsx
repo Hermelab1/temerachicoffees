dd3ac3109e46ab468d4511e09f96cbe388419d86
HermelaB1 <hermelabirhanu06@gmail.com>
2024-12-12 15:38:40 +0300

    Dec 12 2024 updated

--- src/components/Home/hero.jsx
+++ src/components/Home/hero.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import videobg from '../../asset/video/Movie.mp4';
-import Heading from './headings';
+import Heading from './headings'; // Import Heading component
 
 const Hero = () => {
   return (
-    <section className="hero w-full h-screen flex justify-center items-center">
+    <section className="hero relative w-full md:h-[90vh] h-[100vh]">
       <div className="absolute inset-0">
         <video className="w-full h-full object-cover" src={videobg} autoPlay loop muted></video>
-        <div className="absolute inset-0 bg-black bg-opacity-50"></div>
+        <div className="absolute inset-0 bg-black bg-opacity-50"></div> {/* Adjust opacity here */}
       </div>
       <div className="slogan">
         <Heading title="Every beans tells a story" subtitle="Temerachi Coffee Export" />
